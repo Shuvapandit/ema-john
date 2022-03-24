79c44258b38b4bcd24f87322525851fa59aef38b
shuva <chakrabortyshuva216@gmail.com>
2022-03-24 23:32:23 +0600

    product items updated

---
+++ src/components/Product/Product.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart} from '@fortawesome/free-solid-svg-icons'
+import './Product.css'
+
+const Product = (props) => {
+    
+    const{name,img,price,seller,ratings}=props.product;
+    
+    console.log(props)
+   
+    
+    return (
+        
+        <div className='product'>
+            <img src={img} alt=""></img>
+          <div className='product-info'>
+          <p className='product-name'>{name}</p>
+           <p>Price:${price}</p>
+            <p><small>Manufacturer:{seller}</small></p>
+            <p><small>Rating:{ratings} stars</small></p>
+
+          </div>
+          
+          <button onClick={() =>  props.handleAddToCart(props.product)} className='btn-cart'>
+              <p className='btn-text'>Add to cart</p>
+              <FontAwesomeIcon icon={faShoppingCart} ></FontAwesomeIcon>
+              </button>
+          
+        </div>
+    );
+};
+
+export default Product;
